ba47d7729de34ba884f3b2b8fcda51feb932e538
Pritam Debnath <debnathpritam2222@gmail.com>
2024-10-04 15:52:37 +0530

    ingredients done

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6c&bd52*r*j9*6collar^i6l^8c4%y(a*qn(&vz7b8eo(oq_7s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
